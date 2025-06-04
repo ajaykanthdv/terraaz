8596cd0ea0beb393ff83a9985ee893f2bb0725d2
ajaykanth12345 <ajaykanth12345@gmail.com>
2025-06-04 08:24:10 +0000

    updated certs

--- main.tf
+++ main.tf
@@ -18,9 +18,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id                 = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
-  client_id                       = "4813663b-851f-4614-90a7-ef34f0e92b4d"
-  client_secret                   = "QZN8Q~lVsEEfDY~wrMe6gHTugCmtNVRx7SzHdcnh"
+  subscription_id                 = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
+  client_id                       = "8a569dac-3611-40bb-9a77-25d11bc2a48c"
+  client_secret                   = "Ux18Q~StnYml_kXqUTj3pFU7LrRo~mEphVP4oaFg"
   tenant_id                       = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
   resource_provider_registrations = "none"
 }
