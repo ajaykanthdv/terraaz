5bf479fd4ed4b5236218ef9fb84f7fe91663bf2e
ajaykanthdv <104226770+ajaykanthdv@users.noreply.github.com>
2025-06-04 09:15:36 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,6 +37,13 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+resource "azurerm_storage_account" "storage" {
+  name                     = "ajayterrac"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
   tags = {
     environment = "playground"
     owner       = "kloud"
